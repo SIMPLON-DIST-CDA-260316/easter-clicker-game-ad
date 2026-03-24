762ea3378bd0648e91de55d6d2961f22671dbeb3
Your Name <you@example.com>
2026-03-24 16:02:51 +0100

    feat : chrono

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const egg = document.querySelector('.egg');
+const tabImages = [];
+const clic = document.querySelector('.clic');
+const timer = document.querySelector('#timer')
 
 setTimeout(
-    function () {
-        egg.style.opacity = '1';
+    function displayEgg() {
+        egg.style.display = 'block';
     },
-    2000
+    1000
 );
 
 // l'oeuf disparaît au clic
 egg.addEventListener('click', function () {
-    egg.style.opacity = '0';
+    egg.style.display = 'none';
 });
+
+// Compteur de clic
+egg.addEventListener("click", (event) => {
+    clic.textContent = `Nombre de clics : ${event.detail}`;
+});
+
+// Chrono
+const departMinutes = 2;
+let time = departMinutes * 60;
+
+const timerElement = document.getElementById("timer");
+
+setInterval(() => {
+  let minutes = parseInt(time / 60, 10);
+  let secondes = parseInt(time % 60, 10);
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  secondes = secondes < 10 ? "0" + secondes : secondes;
+
+  timerElement.innerText = `Compte à rebours : ${minutes}:${secondes}`;
+  time = time <= 0 ? 0 : time - 1;
+}, 1000);
